e70c7f80d85a57376691159d142cf5cde3fc2835
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-03 13:14:07 -0800

    setup for books framework

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Books from '../Books/Books'
+
 
 const App = () => (
+  <div>
   <h1> hello world </h1>
+  <Books/>
+  </div>
 )
 
 export default App
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Book from '../Book/Book'
+import AddBook from '../AddBook/AddBook'
+
 
 const Books = () => (
+  <div>
   <h1> Books </h1>
+  <Book />
+  <AddBook />
+    </div>
 )
 
 export default Books
+
