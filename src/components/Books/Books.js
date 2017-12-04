eba2a6fd53c90f8087aa0e677d267bf0ccf9ebb1
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-03 18:45:10 -0800

    added state to books with a turnary to display a div

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,15 +1,38 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, PureComponent } from 'react'
 import Book from 'components/Book/Book'
 import AddBook from 'components/AddBook/AddBook'
 
 
-const Books = () => (
-  <Fragment>
-  <h1> Books </h1>
-  <Book />
-  <AddBook />
-    </Fragment>
-)
+class Books extends PureComponent {
+  
+  state = {
+    books: [],
+  }
+
+  render(){
+    const { books } = this.state
+
+    return(
+      <Fragment>
+        {books.length
+         ? (
+            <ul>
+              {books.map((book) => (
+              <Book 
+                {...book}
+                key={book._id}
+              />
+              ))}
+            </ul>
+         )
+         : (
+           <p>You dont have any books</p> 
+         )
+        }
+        <AddBook />
+      </Fragment>
+    )}
+ }
 
 export default Books
 
